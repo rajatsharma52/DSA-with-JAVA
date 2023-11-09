9e207e007bf6160ca01c26f40aac4ceb48295886
Rajat Sharma <132869604+gopalasharma@users.noreply.github.com>
2023-11-09 20:49:23 +0530

    Add files via upload

---
+++ Strings/string_concatenate.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class string_concatenate {
+    public static void main(String[] args) {
+        String firstName= "Rajat";
+        String lastName= "Sharma";
+        String fullName= firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+    
+}
---
+++ Strings/string_length.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class string_length {
+    public static void main(String[] args) {
+        String fullName= "Rajat Sharma";
+        System.out.println("Length of my string is:" +fullName.length());
+    }
+    
+}
