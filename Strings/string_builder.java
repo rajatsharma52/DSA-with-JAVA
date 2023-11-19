0bbe18ca2ba8d5f72ba7441f6354974a8e3ff48d
Rajat Sharma <132869604+gopalasharma@users.noreply.github.com>
2023-11-19 09:05:04 +0530

    Add files via upload

---
+++ Strings/string_builder.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class string_builder {
+    public static void main(String[] args) {
+        StringBuilder sb= new StringBuilder("");
+        for(char ch= 'A'; ch<='Z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+    }
+    
+}
