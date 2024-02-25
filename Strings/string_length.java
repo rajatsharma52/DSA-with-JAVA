1d121334b2bddaace83e201d0a2d5d3e6e8bca05
Rajat Sharma <132869604+gopalasharma@users.noreply.github.com>
2024-02-25 08:39:35 +0530

    Delete Strings directory

--- Strings/string_length.java
+++
@@ -1,9 +0,0 @@
-package Strings;
-
-public class string_length {
-    public static void main(String[] args) {
-        String fullName= "Rajat Sharma";
-        System.out.println("Length of my string is:" +fullName.length());
-    }
-    
-}
