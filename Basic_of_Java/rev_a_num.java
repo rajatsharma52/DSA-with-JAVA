e2022c68f39b5a80c8377dcbf57fa5a8fa313089
Rajat Sharma <132869604+gopalasharma@users.noreply.github.com>
2024-02-25 08:57:38 +0530

    Update rev_a_num.java

--- Basic_of_Java/rev_a_num.java
+++ Basic_of_Java/rev_a_num.java
@@ -7,7 +7,7 @@ public class rev_a_num {
         int n=78374873;
         int rev=0;
 
-        while(n>0){
+        while(n != 0){
             int lastDigit= n%10;
             rev= (rev*10) + lastDigit;
             n=n/10;
