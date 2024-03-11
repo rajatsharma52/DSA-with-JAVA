c70a5ef838447ec34819b7af12f6c01d953864c8
Rajat Sharma <132869604+gopalasharma@users.noreply.github.com>
2024-03-11 22:09:00 +0530

    Add files via upload

---
+++ Recursion_Basics/factorial_of_n.java
@@ -0,0 +1,18 @@
+package Recursion_Basics;
+
+public class factorial_of_n {
+    public static int findFactorial(int n){
+        // Base case
+        if(n == 0){
+            return 1;
+        }
+        int fn_1= findFactorial(n-1);
+        int fn= n*findFactorial(n-1);
+        return fn;
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(findFactorial(n));
+    }
+    
+}
