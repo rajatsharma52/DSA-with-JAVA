2cd6683b550c4a4d85c8ded99ee06489cd46d6c3
Rajat Sharma <132869604+gopalasharma@users.noreply.github.com>
2024-03-21 09:13:23 +0530

    Add files via upload

---
+++ ArrayList/define_arraylist.java
@@ -0,0 +1,23 @@
+package ArrayList;
+
+import java.util.ArrayList;;
+
+public class define_arraylist {
+    public static void main(String[] args) {
+
+        // Defining the arraylists of different type
+        
+        // Integer type
+        ArrayList<Integer> list1= new ArrayList<>();
+
+        // String type
+        ArrayList<String> list2= new ArrayList<>();
+
+        // Float type
+        ArrayList<Float> list3= new ArrayList<>();
+
+        // Boolean type
+        ArrayList<Boolean> list4= new ArrayList<>();
+    }
+    
+}
