1435685651979675c87aef1a0500f9b2005ad215
Rajat Sharma <132869604+rajatsharma52@users.noreply.github.com>
2024-03-25 12:17:13 +0530

    Add files via upload

---
+++ LinkedList/create_a_class_for_node.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class create_a_class_for_node {
+
+    //Creating a class for our nodes
+    public static class Node{
+        int data; //1st part of Node
+        Node next; //2nd part of Node
+
+        //Creating the constructor
+        public Node(int data){
+            this.data= data;
+            this.next= null;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
